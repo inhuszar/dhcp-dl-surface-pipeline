b92b03e6e7c4141fd8c782a71e773dd9e2fcbec1
Istvan N Huszar <ihuszar@mgh.harvard.edu>
2024-07-01 20:58:43 -0400

    Fixed installation file.

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
+#!/bin/bash
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check if "mamba" is available
+if command_exists mamba; then
+    EXECUTABLE="mamba"
+elif command_exists conda; then
+    EXECUTABLE="conda"
+else
+    echo "Neither 'mamba' nor 'conda' executable found in PATH."
+    exit 1
+fi
+
 # create new conda environment
-conda create -n dhcp
-conda activate dhcp
+$EXECUTABLE create -n dhcp -y
+$EXECUTABLE activate dhcp
 
 # install pytorch
-conda install pytorch=1.13.0 pytorch-cuda=11.6 -c pytorch -c nvidia
+$EXECUTABLE install pytorch=1.13.0 pytorch-cuda=11.6 -c pytorch -c nvidia -y
 
 # install dependencies
-pip install tqdm scipy==1.10.1 nibabel==5.0.1 antspyx==0.3.7
+pip install tqdm scipy==1.10.1 nibabel==5.0.1 antspyx==0.3.8
